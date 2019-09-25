e32c07cdff1e8373f34e1f75648cc6c194f41b8c
Michael Solati <mkslt04@gmail.com>
2019-09-25 13:55:33 -0700

    chore: remove and update dependencies

--- src/GeoFirestoreTypes.ts
+++ src/GeoFirestoreTypes.ts
@@ -2,7 +2,6 @@
 import * as FirestoreTypes from '@firebase/firestore-types';
 import '@google-cloud/firestore/types/firestore';
 import '@types/node';
-import * as firebase from 'firebase/app';
 
 export namespace GeoFirestoreTypes {
   export interface Document {
@@ -37,19 +36,19 @@ export namespace GeoFirestoreTypes {
   export interface UpdateData { [fieldPath: string]: any; coordinates?: cloud.GeoPoint | web.GeoPoint; }
   export type WhereFilterOp = '<' | '<=' | '==' | '>=' | '>' | 'array-contains';
   export namespace web {
-    export type CollectionReference = FirestoreTypes.CollectionReference | firebase.firestore.CollectionReference;
-    export type DocumentChange = FirestoreTypes.DocumentChange | firebase.firestore.DocumentChange;
-    export type DocumentReference = FirestoreTypes.DocumentReference | firebase.firestore.DocumentReference;
-    export type DocumentSnapshot = FirestoreTypes.DocumentSnapshot | firebase.firestore.DocumentSnapshot;
-    export type Firestore = FirestoreTypes.FirebaseFirestore | firebase.firestore.Firestore;
-    export type FieldPath = FirestoreTypes.FieldPath | firebase.firestore.FieldPath;
-    export type GetOptions = FirestoreTypes.GetOptions | firebase.firestore.GetOptions;
-    export type GeoPoint = FirestoreTypes.GeoPoint | firebase.firestore.GeoPoint;
-    export type Query = FirestoreTypes.Query | firebase.firestore.Query;
-    export type QueryDocumentSnapshot = FirestoreTypes.QueryDocumentSnapshot | firebase.firestore.QueryDocumentSnapshot;
-    export type QuerySnapshot = FirestoreTypes.QuerySnapshot | firebase.firestore.QuerySnapshot;
-    export type Transaction = FirestoreTypes.Transaction | firebase.firestore.Transaction;
-    export type WriteBatch = FirestoreTypes.WriteBatch | firebase.firestore.WriteBatch;
+    export type CollectionReference = FirestoreTypes.CollectionReference;
+    export type DocumentChange = FirestoreTypes.DocumentChange;
+    export type DocumentReference = FirestoreTypes.DocumentReference;
+    export type DocumentSnapshot = FirestoreTypes.DocumentSnapshot;
+    export type Firestore = FirestoreTypes.FirebaseFirestore;
+    export type FieldPath = FirestoreTypes.FieldPath;
+    export type GetOptions = FirestoreTypes.GetOptions;
+    export type GeoPoint = FirestoreTypes.GeoPoint;
+    export type Query = FirestoreTypes.Query;
+    export type QueryDocumentSnapshot = FirestoreTypes.QueryDocumentSnapshot;
+    export type QuerySnapshot = FirestoreTypes.QuerySnapshot;
+    export type Transaction = FirestoreTypes.Transaction;
+    export type WriteBatch = FirestoreTypes.WriteBatch;
   }
   export namespace cloud {
     export type CollectionReference = FirebaseFirestore.CollectionReference;
